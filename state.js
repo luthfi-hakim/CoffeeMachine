25ed84cac64b5faa9e6ea485893591eeaa79b730
luthfiluqmanulhakim0@gmail.com <luthfiluqmanulhakim0@gmail.com>
2023-11-06 15:16:27 +0700

    first commit

---
+++ state.js
@@ -0,0 +1,11 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+  }
+   
+  const isCoffeeMachineReady = true;
+  // Menginialisasi ulang module.export
+  module.exports = {coffeeStock, isCoffeeMachineReady}; 
+  // module.exports = coffeeStock;
+  // console.log(module);
