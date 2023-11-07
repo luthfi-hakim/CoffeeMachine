d6aa687ce2a212d1acc8808ba67e476fa7c14124
luthfiluqmanulhakim0@gmail.com <luthfiluqmanulhakim0@gmail.com>
2023-11-07 07:44:45 +0700

    ES6 Module : export import module

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const coffeeStock = require('./state');
+// //
+// // Destructuring 1 nilai pada berkas javascript
+// //
+// import coffeeStock from './state.js';
+// // Menulis tiap tipe kopi   
+// const displayStock = stock => {
+//     for (const type in stock) {
+//         console.log(type);
+//     }
+// }
+
+// displayStock(coffeeStock);
+
+//
+// Destructuring banyak  nilai pada berkas javascript
+//
+
+import { coffeeStock, isCoffeeMachineReady } from './state.js';
+ 
+console.log(coffeeStock);
+
+// Menulis tiap tipe kopi   
+const displayStock = stock => {
+    for (const type in stock) {
+        console.log(type);
+    }
+}
+
+displayStock(coffeeStock);
+
+console.log(isCoffeeMachineReady);
+
+
+// const coffeeStock = require('./state');
  
 // console.log(coffeeStock);
 
-const makeCoffee = (type, miligrams) => {
-    if (coffeeStock[type] >= miligrams) {
-      console.log("Kopi berhasil dibuat!");
-    } else {
-      console.log("Biji kopi habis!");
-    }
-  }
+// const makeCoffee = (type, miligrams) => {
+//     if (coffeeStock[type] >= miligrams) {
+//       console.log("Kopi berhasil dibuat!");
+//     } else {
+//       console.log("Biji kopi habis!");
+//     }
+//   }
    
-  makeCoffee("robusta", 80);
+//   makeCoffee("robusta", 80);
--- state.js
+++ state.js
@@ -1,11 +1,21 @@
 const coffeeStock = {
-    arabica: 100,
-    robusta: 150,
-    liberica: 200
-  }
+  arabica: 100,
+  robusta: 150,
+  liberica: 200
+};
+
+const isCoffeeMachineReady = true;
+
+// Mengirim 1 nilai 
+// export default coffeeStock;
+
+//Mengirim banyak nilai
+export {coffeeStock, isCoffeeMachineReady};
    
-  const isCoffeeMachineReady = true;
-  // Menginialisasi ulang module.export
-  module.exports = {coffeeStock, isCoffeeMachineReady}; 
-  // module.exports = coffeeStock;
-  // console.log(module);
+
+
+// Menginialisasi ulang module.export
+// module.exports = {coffeeStock, isCoffeeMachineReady}; 
+
+// module.exports = coffeeStock;
+// console.log(module);
